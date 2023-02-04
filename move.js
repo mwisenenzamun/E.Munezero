31174e551fb9620545d93c0872db3c290c1642f6
mwisenenzamun <80822746+mwisenenzamun@users.noreply.github.com>
2023-02-04 19:17:37 +0100

    Add files via upload

---
+++ move.js
@@ -0,0 +1,59 @@
+/* function grand() {
+    var x = document.getElementById("slides1"); /*carrousel
+    var o = document.getElementById("slides2");
+    var h = document.getElementById("slides3");
+    var k = document.getElementById("slides4");
+    var l = document.getElementById("slides5");
+    /*var y = document.getElementById("bout"); /*imge
+    var z = document.getElementById("bouti");
+    var e = document.getElementById("bouti1");
+    var t = document.getElementById("bouti2");
+    var g = document.getElementById("bouti3");
+    var n = document.getElementById("bout");
+
+    if (g.style.display == "none") {
+        console.log("yes");
+
+        x.style.width = "100%"
+        o.style.width = "100%"
+        h.style.width = "100%"
+        k.style.width = "100%"
+        l.style.width = "100%"
+
+        z.style.display = "none"
+
+        g.style.display = "flex"
+
+        e.style.display = "none"
+
+        g.style.display = "flex"
+
+        t.style.display = "none"
+        n.style.display = "none"
+
+
+    } else {
+        x.style.width = "70%"
+        o.style.width = "70%"
+        h.style.width = "70%"
+        k.style.width = "70%"
+        l.style.width = "70%"
+
+
+        z.style.display = "flex"
+        g.style.display = "none"
+
+        e.style.display = "flex"
+
+        g.style.display = "none"
+        t.style.display = "flex"
+
+        n.style.display = "flex"
+
+    }
+
+
+*/
+
+
+
